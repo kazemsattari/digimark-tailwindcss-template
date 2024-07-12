f27c5fd44c8e2e00da2ffd84f427e75aa653c9c0
kazemsattari <kazem.sattari@gmail.com>
2024-07-12 04:02:02 -0700

    initial DigiMark project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Vazir: "Vazir",
+      VazirFD: "Vazir-FD",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
